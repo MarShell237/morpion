8691b8bd1fa230e5c66fe8fec396763f6029426e
MarShell237 <marcelj.djiofack@outlook.com>
2026-06-24 02:12:42 +0100

    add draw content cell function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rusty_engine::prelude::*;
 
 #[derive(Resource)]
 struct GameState {
-    health_left: i32,
+    grid: [[(u8, Vec2); 3]; 3],
 }
 
 fn main() {
@@ -17,5 +17,48 @@ fn main() {
     let line4 = game.add_sprite("line4", "sprite/separator.png");
     line4.translation.y = 66.66;
     line4.rotation = UP;
-    game.run(GameState { health_left: 42 });
+    // let o1 = game.add_text("o1", "o");
+    // o1.translation.x = -133.33;
+    // o1.translation.y = 133.33;
+    // o1.font_size = 100.0;
+    // let x1 = game.add_text("x1", "x");
+    // x1.font_size = 100.0;
+    let grid: [[(u8, Vec2); 3]; 3] = [
+        [
+            (1, Vec2::new(-133.33, 133.33)),
+            (0, Vec2::new(0.0, 133.33)),
+            (1, Vec2::new(133.33, 133.33)),
+        ],
+        [
+            (0, Vec2::new(-133.33, 0.0)),
+            (2, Vec2::new(0.0, 0.0)),
+            (0, Vec2::new(133.33, 0.0)),
+        ],
+        [
+            (2, Vec2::new(-133.33, -133.33)),
+            (0, Vec2::new(0.0, -133.33)),
+            (0, Vec2::new(133.33, -133.33)),
+        ],
+    ];
+    game.add_logic(game_logic);
+    game.run(GameState { grid });
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    for (i, line) in game_state.grid.iter().enumerate() {
+        for (j, cell) in line.iter().enumerate() {
+            let mut cell_content_string = String::new();
+            if cell.0 == 1 {
+                cell_content_string = "o".to_string();
+            }
+
+            if cell.0 == 2 {
+                cell_content_string = "x".to_string();
+            }
+
+            let cell_content = engine.add_text(format!("cell{}{}", i, j), cell_content_string);
+            cell_content.translation = cell.1;
+            cell_content.font_size = 100.0;
+        }
+    }
 }
